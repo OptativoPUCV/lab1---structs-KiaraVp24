ff3c564c863082e75d963441a0ad325154d92ee3
Your Name <you@example.com>
2024-03-16 17:02:45 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -26,7 +26,7 @@ invierta el orden de sus elementos.
 */
 void reverseArray(int arr[], int size) {
   int temp;
-  for (int i = 0; i < size - 1; i++) {
+  for (int i = 0; i < size / 2; i++) {
     temp = arr[i];
     arr[i] = arr[size - 1 - i];
     arr[size - 1 - i] = temp;
